c4f9d58ae7fe275875b4b9be4955c68cd731243a
ABEL\Jeff <jeff@schaefersoftware.com>
2018-11-03 06:09:54 -0400

    added more advanced line number detection

--- debug.php
+++ debug.php
@@ -1,15 +1,18 @@
 <?php
 	function dbg(){
 		$str = '';
+		$backtrace = debug_backtrace();
+		
+		$str .= '<div style="text-align:left">';
 		$args = func_get_args();
+		$str .= '<h1>'.$backtrace[1]['class'].'::'.$backtrace[1]['func'].' line '.$backtrace[0]['line'].'</h1>';
 		foreach($args as $k=>$arg){
-			$str .= '<h2>Debug Param '.$k.'</h2><pre>';
+			$str .= '<h2>Arg '.$k.'</h2><pre>';
 			$str.= print_r($arg,true);
 			$str.= '</pre><hr>';
 		}
-		$str .= '<hr>';
+		$str .= '</div>';
 		echo $str;
-		
 	}
 	
 ?>
